f82442782402ba8e2891601f3ddb9fd146f66cd8
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-06-06 12:25:04 +0200

    Add files via upload

---
+++ zavrsnoPonavljanje2/zavrsnoPonavljanje2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsnoPonavljanje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sek, min, vrijeme;
+
+            Console.Write("Upišite broj sekundi: ");
+            vrijeme = Convert.ToInt32(Console.ReadLine());
+
+            min = vrijeme / 60;
+            sek = vrijeme % 60;
+
+            Console.Write("Vrijeme pretvoreno u minute i sekunde je: " + min + ":" + sek);
+
+            Console.ReadKey();
+        }
+    }
+}
